dfc8380a4b7be37aef2e604242486b2baa1ec2d8
550728110@qq.com <GoesJ1996>
2018-11-10 23:26:43 +0800

    完成统计各攻击信息和武器信息综合的人员伤亡情况

--- Data.py
+++ Data.py
@@ -1,10 +1,17 @@
 from xlrd import *
 import numpy as np
 
+global NUM_TYPE_WEAPON
+global NUM_TYPE_ATTACK
+NUM_TYPE_WEAPON = 13  # 武器信息种类数量
+NUM_TYPE_ATTACK = 9  # 攻击信息种类数量
+
 
 # 用于打开文件，保存文件实例
 class Data:
+
     def __init__(self, file_path):
+
         self.data = None
         self.table = None
         try:
@@ -24,45 +31,27 @@ class Data:
         return list(self.table.col_values(m, start_rowx=1, end_rowx=None))
 
     # 统计各攻击信息和武器信息综合的人员伤亡情况
+    # 返回伤亡情况数组： 2 * 武器信息种类 * 攻击信息种类
     def get_casualty_by_attack_and_weapon(self):
         death_list = self.get_data_list('nkill')
         print("death_list length: " + str(len(death_list)))
         wound_list = self.get_data_list('nwound')
         print("wound_list length: " + str(len(wound_list)))
-        attack_list = self.get_data_list('attacktype1')
-        print("attack1_list length: " + str(len(attack_list)))
-        weapon_list = self.get_data_list('weaptype1')
-        print("weapon1_list length: " + str(len(weapon_list)))
-        # 伤亡情况数组为 2 * 武器信息种类 * 攻击信息种类
-        casualty_array = np.zeros((2, len(weapon_list), len(attack_list)))
-        # TODO: 后期补充后此处两个list不应该存在空元素，可以删掉判断
-        for i in range(len(death_list)):
-            if death_list[i] != '':
-                casualty_array[0, int(attack_list[i]), int(weapon_list[i])] += int(death_list[i])
-            if wound_list[i] != '':
-                casualty_array[1, int(attack_list[i]), int(weapon_list[i])] += int(wound_list[i])
-        attack_list.clear()
-        weapon_list.clear()
-        attack_list = self.get_data_list('attacktype2')
-        print("attack2_list length: " + str(len(attack_list)))
-        weapon_list = self.get_data_list('weaptype2')
-        print("weapon2_list length: " + str(len(weapon_list)))
-        for i in range(len(death_list)):
-            if death_list[i] != '':
-                if attack_list[i] != '':
-                    if weapon_list[i] != '':
-                        casualty_array[0, int(attack_list[i]), int(weapon_list[i])] += int(death_list[i])
-            if wound_list[i] != '':
-                casualty_array[1, int(attack_list[i]), int(weapon_list[i])] += int(wound_list[i])
-        attack_list.clear()
-        weapon_list.clear()
-        attack_list = self.get_data_list('attacktype3')
-        print("attack3_list length: " + str(len(attack_list)))
-        weapon_list = self.get_data_list('weaptype3')
-        print("weapon3_list length: " + str(len(weapon_list)))
-        for i in range(len(death_list)):
-            if death_list[i] != '':
-                casualty_array[0, int(attack_list[i]), int(weapon_list[i])] += int(death_list[i])
-            if wound_list[i] != '':
-                casualty_array[1, int(attack_list[i]), int(weapon_list[i])] += int(wound_list[i])
+        casualty_array = np.zeros((2, NUM_TYPE_WEAPON + 1, NUM_TYPE_ATTACK + 1))
+        for j in range(3):
+            attack_list = self.get_data_list('attacktype' + str(j + 1))
+            print("attack_list length: " + str(len(attack_list)))
+            weapon_list = self.get_data_list('weaptype' + str(j + 1))
+            print("weapon_list length: " + str(len(weapon_list)))
+            # TODO: 后期补充后此处两个list不应该存在空元素，可以删掉判断
+            for i in range(len(death_list)):
+                if attack_list[i] != '' and weapon_list[i] != '':
+                    if death_list[i] != '':
+                        casualty_array[0, int(weapon_list[i]), int(attack_list[i])] = \
+                            casualty_array[0][int(weapon_list[i])][int(attack_list[i])] + int(death_list[i])
+                    if wound_list[i] != '':
+                        casualty_array[1, int(weapon_list[i]), int(attack_list[i])] = \
+                            casualty_array[1][int(weapon_list[i])][int(attack_list[i])] + int(wound_list[i])
+            attack_list.clear()
+            weapon_list.clear()
         return casualty_array
--- terrors_classification.py
+++ terrors_classification.py
@@ -3,12 +3,9 @@ from Data import *
 
 def init_data():
     print("System started.")
-    file_path = 'F:/PycharmProjects/stock_prediction/competition topic/测试数据.xlsx'
+    file_path = './competition topic/测试数据.xlsx'
     local_data = Data(file_path)
     return local_data
 
 
 data = init_data()
-casualty_array = data.get_casualty_by_attack_and_weapon()
-print(casualty_array[0].size())
-print(casualty_array[1].size())

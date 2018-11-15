010711567b2f6152f01a26ab86d48cad67c15f56
550728110@qq.com <GoesJ1996>
2018-11-15 17:09:00 +0800

    最终完成第一题

--- terrors_classification.py
+++ terrors_classification.py
@@ -3,7 +3,7 @@ from Data import *
 
 def init_data():
     print("System started.")
-    file_path = r'./competition topic/导出数据new.xlsx'
+    file_path = r'./competition topic/处理后数据.xlsx'
     local_data = Data(file_path)
     return local_data
 
@@ -12,4 +12,6 @@ data = init_data()  # 创建实例
 # data.initial_data()  # 填充数据
 # data.get_matrix_attacktype_score()
 # data.get_matrix_weapontype_score()
-data.insert_score()
+# data.insert_score()
+data.cal_total_F()
+print("finish!!!")
